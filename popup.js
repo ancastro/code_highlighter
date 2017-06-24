7fce55cd0aa330eddb614aa195f4a1b28a54404e
Andrew Castro <andrewcastro5@gmail.com>
2017-06-24 12:49:06 -0400

    Add comments on script

--- popup.js
+++ popup.js
@@ -2,20 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
   
   function addHighlight() {
     const code = `
+      // Get the head of our page 
       var head = document.getElementsByTagName('head')[0];
       
+      // Create the stylesheet for Highlightjs (Sublime Theme)
+      // <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css" id="highlight-css">
       var highlight_stylesheet = document.createElement("link");
       highlight_stylesheet.rel = "stylesheet"
       highlight_stylesheet.href = "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css"
       highlight_stylesheet.id = "highlight-css"
       
+      // Create the script for Highlightjs
+      // <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js" id="highlight-js"></script>
       var highlight_script = document.createElement("script");
       highlight_script.src = "//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"
       highlight_script.id = "highlight-js"
       
+      // Add the Highlightjs script and stylesheet to the head of our document
       head.appendChild(highlight_stylesheet);
       head.appendChild(highlight_script);
       
+      // When the script loads we need to run initHighlighting to create the syntax highlighting on our page
       highlight_script.onload = function() {
         var start_highlight = document.createElement('script');
         start_highlight.innerHTML = "hljs.initHighlighting();"
